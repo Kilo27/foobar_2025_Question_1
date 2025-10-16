e039c748a7b1c47e387d4a55eda4c7775a5d864b
Ragefester <141954356+ragefester@users.noreply.github.com>
2025-10-16 10:49:18 +0100

    added pandas df

--- detectives/main.py
+++ detectives/main.py
@@ -1,1 +1,16 @@
 # This is where the main logic will goes
+
+import pandas as pd
+import numpy as np
+
+# Load all CSV files
+transactions = pd.read_csv('user_materials/Transactions.csv')
+users = pd.read_csv('user_materials/Users.csv')
+hosts = pd.read_csv('user_materials/Hosts.csv')
+blacklist = pd.read_csv('user_materials/Blacklist.csv')
+notes = pd.read_csv('user_materials/Notes.csv')
+devices = pd.read_csv('user_materials/Devices.csv')
+
+# Display as dataframes/tables
+print("Transactions:")
+print(transactions.head())
